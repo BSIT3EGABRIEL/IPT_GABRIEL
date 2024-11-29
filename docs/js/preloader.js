5c15a50267b089f301ee708d7479449d3f3715cf
BSIT3E_GABRIEL <202202861@cityofmalabonuniversity.edu.ph>
2024-11-29 22:18:48 +0800

    PORTFOLIO

--- docs/js/preloader.js
+++ docs/js/preloader.js
@@ -1,14 +1,8 @@
-window.onload = function () {
-    // Select the preloader and main content
-    const preloader = document.querySelector('.preloader');
-    const content = document.querySelector('.container');
 
-    // Add a fade-out class to the preloader
-    preloader.classList.add('fade-out');
+    document.addEventListener("DOMContentLoaded", () => {
 
-    // Show the main content after the fade-out animation
-    setTimeout(() => {
-        preloader.style.display = 'none'; // Hide the preloader
-        content.style.display = 'block'; // Show the main content
-    }, 5000); // Match the fade-out animation duration
-};
+  setTimeout(() => {
+    document.body.classList.add('loaded');
+  }, 3000); 
+});
+
